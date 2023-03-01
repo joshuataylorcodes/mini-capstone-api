5cdbab0ce7d2c9194d9b9e51e822311d8d29b42e
joshua <joshuamtaylor455@gmail.com>
2023-03-01 13:42:52 -0500

    creating Product Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.new({ name: "Nike Airforce 1", price: 15, image_url: "url", description: "Fresh shoes" })
+product.save
+product = Product.new({ name: "Adidas Sambas", price: 20, image_url: "url", description: "Indoor Soccer shoes" })
+product.save
+product = Product.new({ name: "Puma Cortez", price: 10, image_url: "url", description: "Casual Shoes" })
+product.save
