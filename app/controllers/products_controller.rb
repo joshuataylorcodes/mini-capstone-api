c503652d7ad3e8c494cffab9819b89cc19ea7b2c
joshua <joshuamtaylor455@gmail.com>
2023-03-01 14:04:58 -0500

    Routes to display individual products which is inconvenient

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,19 @@
 class ProductsController < ApplicationController
+  def first_product
+    @product = Product.first
+    render template: "products/show"
+  end
+
+  def second_product
+    @product = Product.second
+    render template: "products/show"
+  end
+
+  def third_product
+    @product = Product.last
+    render template: "products/show"
+  end
+
   def all_products
     @products = Product.all
     render template: "products/index"
