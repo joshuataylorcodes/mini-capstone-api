66816fbb8ebe224a4dc53cb967da905cb216e44e
joshua <joshuamtaylor455@gmail.com>
2023-03-02 13:47:54 -0500

    Implement the restful index and show actions in your mini-capstone

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,11 @@
 class ProductsController < ApplicationController
-  def one_product
+  def show
     @product = Product.find_by(id: params[:id])
-    render template: "products/show"
+    render :show
   end
 
-  def second_product
-    @product = Product.second
-    render template: "products/show"
-  end
-
-  def third_product
-    @product = Product.last
-    render template: "products/show"
-  end
-
-  def all_products
+  def index
     @products = Product.all
-    render template: "products/index"
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get "/all_products", controller: "products", action: "all_products"
+  get "/products" => "products#index"
 
-  get "/one_product/:id", controller: "products", action: "one_product"
-
-  get "/second_product", controller: "products", action: "second_product"
-
-  get "/third_product", controller: "products", action: "third_product"
+  get "/products/:id" => "products#show"
 end
