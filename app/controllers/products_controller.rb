c1d9f7a0191aeb8cd5b32c3d9532108ecab3bf0c
joshua <joshuamtaylor455@gmail.com>
2023-03-06 15:30:50 -0500

    adding tests and creating models to run those tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,15 +9,15 @@ class ProductsController < ApplicationController
     render :index
   end
 
-  # def create
-  #   @product = Product.create(
-  #     name: "Ecco 1's",
-  #     price: 50,
-  #     image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.la-botte.com%2F4DCGI%2Fvoir_us%2Fsoft-7-navy-blue%2Fshoes%2Fmen%2Fsneakers%2Fecco%2F&psig=AOvVaw3lwJYUOW9LdEiKhG1J3OCf&ust=1677872391734000&source=images&cd=vfe&ved=0CBAQjhxqFwoTCKi3j_H_vf0CFQAAAAAdAAAAABAH",
-  #     description: "casual shoes that fit with most semiformal and casual outfits",
-  #   )
-  #   render :show
-  # end
+  def create
+    @product = Product.create(
+      name: "Ecco 1's",
+      price: 50,
+      image_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.la-botte.com%2F4DCGI%2Fvoir_us%2Fsoft-7-navy-blue%2Fshoes%2Fmen%2Fsneakers%2Fecco%2F&psig=AOvVaw3lwJYUOW9LdEiKhG1J3OCf&ust=1677872391734000&source=images&cd=vfe&ved=0CBAQjhxqFwoTCKi3j_H_vf0CFQAAAAAdAAAAABAH",
+      description: "casual shoes that fit with most semiformal and casual outfits",
+    )
+    render :show
+  end
 
   def create
     @product = Product.create(
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    x = false
+    if price <= 10
+      x = true
+    end
+    return x
+  end
+
+  def tax
+    tax = price * 0.09
+  end
+
+  def total
+    total = price + tax
+  end
 end
