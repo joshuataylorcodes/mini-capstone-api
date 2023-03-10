50826d636ee895618e949737d9c54180b0a53fa3
joshua <joshuamtaylor455@gmail.com>
2023-03-10 16:01:44 -0500

    add jbuilder for orders, edit create action for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def create
-    order = Order.new(
+    @order = Order.new(
       user_id: params[:user_id],
       product_id: params[:product_id],
       quantity: params[:quantity],
@@ -8,10 +8,24 @@ class OrdersController < ApplicationController
       tax: params[:tax],
       total: params[:total],
     )
-    if order.save
-      render json: { message: "Order was created sucessfuly" }, status: :created
-    else
-      render json: { errors: order.errors.full_messages }, status: :bad_request
+    # if @order.save
+    #   render json: { message: "Order was created sucessfuly" }, status: :created
+    # else
+    #   render json: { errors: order.errors.full_messages }, status: :bad_request
+    # end
+    @order.save
+    render :show
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    render :show
+  end
+
+  def index
+    def index
+      @order = Order.all
+      render :index
     end
   end
 end
