96bdd4d37a888bdf8684b5d0f1c35444c23317e6
joshua <joshuamtaylor455@gmail.com>
2023-03-13 12:41:05 -0400

    add calculations in orders instead of having to give subtotal,tax,and total

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,18 @@
 class OrdersController < ApplicationController
   def create
+    product = Product.find_by(id: params[:product_id])
+
+    calc_subtotal = product.price * params[:quantity].to_i
+    calc_tax = calc_subtotal * 0.09
+    calc_total = calc_subtotal + calc_tax
+
     @order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total],
+      subtotal: calc_subtotal,
+      tax: calc_tax,
+      total: calc_total,
     )
     # if @order.save
     #   render json: { message: "Order was created sucessfuly" }, status: :created
